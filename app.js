6c15b612ae861387ffa1d4dada07e48dc95f05ca
R-Almayyas <108932239+RashedAlmayyas@users.noreply.github.com>
2022-08-23 09:55:32 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+/*
+const createList = (availableItems) => {
+  let arr = [];
+  availableItems.forEach(function(e){if (e.available)arr.push(e)});
+  return arr;
+}
+let a = [
+  { name: 'apples', available: true },
+  { name: 'pears', available: true },
+  { name: 'oranges', available: false },
+  { name: 'bananas', available: true },
+  { name: 'blueberries', available: false }
+];
+console.log(createList(a));
+
+const eo = number => {
+    return number % 2 === 0 ? 'Even' : 'Odd';
+  };
+  
+  console.log(eo(0)); 
+  console.log(eo(2)); 
+  console.log(eo(3)); 
+  console.log(eo(-3)); 
+let arr1=[1,2,3,5];
+let arr2= [1,2,3,4];
+filtered = arr2.filter(
+    function(e) {
+      return this.indexOf(e) < 0;
+    },
+    arr1
+);
+console.log(filtered);
+
+function reverse(str){
+    let reversed = " ";    
+    for (var i = str.length - 1; i >= 0; i--){        
+      reversed += str[i];
+    }    
+    return reversed;
+  }
+  reverse("Rashed");
+
+
+
+const str = '88';
+
+const regex = new RegExp([a-zA-Z+0-9]);
+console.log(regex.test(str));
+*/
